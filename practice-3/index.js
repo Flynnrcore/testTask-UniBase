7ffa7cb8f6b9d5ce8d35b661a113767a8f2e146a
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-09-21 22:30:53 +0300

    fix bugs

--- practice-3/index.js
+++ practice-3/index.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const script = document.createElement('script');
-  script.src = '../practice-2/fetch.js';
-  document.querySelector('body').append(script);
+const sortTable = (columnIndex) => {
+  const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.querySelectorAll('td')[columnIndex].textContent;
+    const cellB = rowB.querySelectorAll('td')[columnIndex].textContent;
+    return cellA.localeCompare(cellB);
+  });
+
+  tableBody.innerHTML = '';
+  rows.forEach(row => {
+    tableBody.appendChild(row);
+  });
+};
+
+const tableCells = document.querySelectorAll('th');
+
+tableCells.forEach((cell, index) => {
+  cell.addEventListener('click', () => {
+    sortTable(index);
+  });
 });
