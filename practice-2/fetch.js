716d3fd9bce40faf9b0c5ee9de6cff3555d8848b
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-09-21 18:11:53 +0300

    fix error 2 and 3 tasks

--- practice-2/fetch.js
+++ practice-2/fetch.js
@@ -1,35 +1,33 @@
-function fetch() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-      const table = document.createElement('table');
-      const tableHeader = document.createElement('thead');
-      const tableBody = document.createElement('tbody');
+const fetchData = async () => {
+  const table = document.createElement('table');
+  const tableHeader = document.createElement('thead');
+  const tableBody = document.createElement('tbody');
+  const headerRow = document.createElement('tr');
 
-      const headerRow = document.createElement('tr');
-      for (let key in data[0]) {
-        const headerCell = document.createElement('th');
-        headerCell.textContent = key;
-        headerRow.appendChild(headerCell);
-      }
-    
-      tableHeader.appendChild(headerRow);
-      table.appendChild(tableHeader);
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
 
-      data.forEach(post => {
-        const row = document.createElement('tr');
-        for (let key in post) {
-          const cell = document.createElement('td');
-         cell.textContent = post[key];
-          row.appendChild(cell);
-        }
-        tableBody.appendChild(row);
-      });
+  for (let key in data[0]) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = key;
+    headerRow.appendChild(headerCell);
+  }
 
-      table.appendChild(tableBody);
-      document.body.appendChild(table)
-    })
-    .catch(error => console.log(error));
-}
+  tableHeader.appendChild(headerRow);
+  table.appendChild(tableHeader);
 
-fetch();
+  data.forEach(post => {
+    const row = document.createElement('tr');
+    for (let key in post) {
+      const cell = document.createElement('td');
+      cell.textContent = post[key];
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  });
+
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
+};
+
+fetchData();
---
+++ practice-3/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const script = document.createElement('script');
+  script.src = '../practice-2/fetch.js';
+  document.querySelector('body').append(script);
+});
